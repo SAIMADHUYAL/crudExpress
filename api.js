5aee9ad6f0a83ef4753d64ef99c0d9b76a351a83
SAIMADHUYAL <saimadhu.yal@gmail.com>
2021-05-05 11:36:53 -0500

    commit

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 var db = mongoose.connect('mongodb://localhost:27017/tcsdb');
+var user = require('./models/usermodel');
 
 
 
